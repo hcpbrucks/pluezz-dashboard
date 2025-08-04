6729a28bb575c2957ad1e1f38ecde2534ad22f32
hcpbrucks <bruckspaul09@gmail.com>
2025-08-05 00:27:13 +0100

    Update app.py

--- pluezz-dashboard/app.py
+++ pluezz-dashboard/app.py
@@ -127,4 +127,5 @@ def admin():
     return render_template("admin.html", status=status, dienste=dienste)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
